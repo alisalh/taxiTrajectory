928443d3697410f78b1a9c1fffb845f4defc7c11
alisalh <508091602@qq.com>
2019-07-29 13:57:33 +0800

    feat: 修复search，只按照name进行搜索

--- static/views/main.js
+++ static/views/main.js
@@ -8,24 +8,12 @@ var topic_colors = ['#1f78b4', '#33a02c', '#fb9a99', '#e31a1c', '#cab2d6',
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am',
     '12pm', '1pm', '2pm', '3pm', '4pm', '5pm',
     '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
+// topic id
 var topics = Array.apply(null, Array(num_topics)).map(function (_, i) {
     return 'Topic ' + (i + 1);
 });
 
-$("#min_support").slider({
-    // tooltip: 'always'
-})
-
-$("#min_confidence").slider({
-	// tooltip: 'always'
-});
-$("#min_trajectory_length").slider({
-	// tooltip: 'always'
-});
-$("#topic_threshold").slider({
-	// tooltip: 'always'
-});
-
+// the value of input changes as the slider 
 $("#min_support").slider().on("slide", function(){
     $("#min_support_input").val($("#min_support").slider('getValue'))});
 $("#min_confidence").slider().on("slide", function(){
@@ -35,19 +23,15 @@ $("#min_trajectory_length").slider().on("slide", function(){
 $("#topic_threshold").slider().on("slide", function(){
     $("#topic_threshold_input").val($("#topic_threshold").slider('getValue'))});
 
-
-// $('.dropdown').on('show.bs.dropdown', function (event) {
-//     var x = $(event.relatedTarget).text(); // Get the button text
-//     alert("You clicked on: " + x);
-// });
 function sortchange(event) {
-    var x = $(this).text()+"<span class='caret'></span>"; // Get the button text
-    $(".dropdown button").html(x);
+    var x = $(this).text()+" <span class='caret'></span>"; //span is the arrow
+    $(".dropdown button").html(x);  // show the selected text
      $.post('/sortphrase/', {"method": $(this).text()}, function (jsondata) {
          create_sub_trajectory_table(table_data, jsondata.data.data);
      });
 }
 $(".dropdown-menu li a").on('click', sortchange);
+
 $("#searchbtn").on('click', function () {
    var search_content = $("#search_content").val();
    if(search_content !== ""){
@@ -57,9 +41,10 @@ $("#searchbtn").on('click', function () {
    }
 });
 
+// search only by name
 $("#search_content").on('change', function () {
     if($(this).val()===''){
-         $.post('/sortphrase/', {"method": $(".dropdown button").text().trim()}, function (jsondata) {
+         $.post('/sortphrase/', {"method": 'name'}, function (jsondata) {
              create_sub_trajectory_table(table_data, jsondata.data.data);
          });
     }
